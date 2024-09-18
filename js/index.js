d5e8c5ffd6c9eecdde485c05da38708ad426b0a5
Tahfimul Latif <victory.josh.josh24@gmail.com>
2024-09-18 18:13:42 +0000

    Added index.js script and attached on click listeners to each animal button

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const animals =
+{
+    0: "Polar Bear",
+    1: "Koala",
+    2: "Kangaroo",
+    3: "Dog",
+    4: "Cat",
+    5: "Lizard",
+    6: "Cow",
+    7: "Horse"
+}
+
+function switch_(animalId)
+{
+    const animals_keys = Object.keys(animals)
+    if(animals_keys.find(animalId)!=-1)
+        window.alert(animals[animalId])
+}
